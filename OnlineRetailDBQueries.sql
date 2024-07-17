4adc0cb1b964a7fc79265114d861513ff313637e
Abhijeet Kumar <116643637+A-bhik@users.noreply.github.com>
2024-07-17 10:34:40 +0530

    Update OnlineRetailDBQueries.sql

--- OnlineRetailDBQueries.sql
+++ OnlineRetailDBQueries.sql
@@ -1,6 +1,6 @@
 /*
 ==================================================================================
-We'll develop a project for a "Fictional Online Retail Company". 
+i have developed a project for a "Fictional Online Retail Company". 
 This project will cover creating a database, tables, and indexes, inserting data,
 and writing various queries for reporting and data analysis.
 ==================================================================================
